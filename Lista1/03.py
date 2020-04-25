e5cd518ffb338896ff7bdf2f63319f5849d9f02d
José Neto <jose.neto@ej4bytes.com.br>
2020-04-25 11:16:45 -0300

    Lista 1 complete

--- Lista1/03.py
+++ Lista1/03.py
@@ -1,3 +1,18 @@
 #Faça um programa para ler a nota da prova de 15 alunos e armazene num vetor,
 #calcule e imprima a média geral.
 
+import random as r
+
+#Define list length
+listX = [0] * 15
+
+#Generating random list
+for i in range(len(listX)):
+    rand_number = r.randint(0,10)
+    listX[i] = rand_number
+
+#Calculating mean
+mean = (sum(listX))/len(listX)
+
+#printing
+print('As notas são: {} \nA média das notas é igual a {}'.format(listX, mean))
